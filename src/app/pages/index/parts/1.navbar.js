8fc4a51a2880e5d74e2badc9f7ab768dec76abad
DaGuT <mangustalex11@gmail.com>
2018-04-16 12:49:23 +0700

    changed navbar to be component + added non used components()

--- src/app/pages/index/parts/1.navbar.js
+++ src/app/pages/index/parts/1.navbar.js
@@ -1,7 +1,7 @@
 import React, { Component}  from 'react';
-import $ from 'jquery';
-import Slideout from "slideout";
 import * as spongeGif from '../../../../img/sb.gif';
+import $ from 'jquery';
+import Navbar from '../../../../misc/components/navbar/navbar';
 
 let params = {
   'displayName': 'Navigation bar',
@@ -15,28 +15,7 @@ let params = {
  * @return {DOM block}  whole navigation block
  */
 function navbar() {
-  return (<nav id={params.blockID} key={params.blockID} className="navbar navbar-expand-md navbar-light bg-light">
-    <a className="navbar-brand" href="#">DaGuT.Ru</a>
-    <button className="navbar-toggler" type="button">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-
-    <div className="collapse navbar-collapse" id="my-menu">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <a className="nav-link" href="#">DaGuT.Ru</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link 1</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Link 2</a>
-        </li>
-      </ul>
-      <ul className="navbar-nav mr-auto" id="blockLinks">
-      </ul>
-    </div>
-  </nav>);
+  return (<Navbar params={params} brand='DaGuT.Ru' linksList={[{link:'#',name:'Link 1'},{link:'#',name:'Link 2'}]} />);
 }
 
 /**
@@ -44,17 +23,7 @@ function navbar() {
  *
  */
 function later() {
-  //sideout place
-  $('#root').before('<sidebar id="side-menu" class="bg-light"><nav class="sidebar" id="sidebar-nav"><img src=' + spongeGif + ' class="img-thumbnail"></nav></sidebar>');
-  //initial sideout
-  var slideout = new Slideout({'panel': document.getElementById('root'), 'menu': document.getElementById('side-menu'), 'padding': 256, 'tolerance': 70, 'side': 'right'});
-  //copy top-menu items to side panel
-  $('#my-menu').children().children().clone().appendTo("#sidebar-nav");
-  $('#side-menu').children().wrapAll("<div class='sidebar-sticky'></div>").wrapAll("<ul class='nav flex-column'></ul>");
-  // Toggle button
-  document.querySelector('.navbar-toggler').addEventListener('click', function() {
-    slideout.toggle();
-  });
+  $('#sidebar-nav').prepend('<img src=' + spongeGif + ' class="img-thumbnail">');
 }
 
 export {params}
