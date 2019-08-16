c6312ac34c3a6803b0486e10b391960e61ba560d
DaGuT <mangustalex11@gmail.com>
2019-08-16 13:38:26 +0100

    added grid style for mobiles

--- src/misc/components/deck/deckitem.js
+++ src/misc/components/deck/deckitem.js
@@ -5,16 +5,28 @@ import "./grid.css";
 
 class Deckitem extends Component {
   render() {
-    return (<div href={this.props.item.src} className={`grr-${this.props.item.rows || 1} grc-${this.props.item.cols || 1} pf-item with-overlay mf-popup`} style={{
-      'backgroundImage' : `url(${this.props.item.imgSrc})`
+    return (
+      <div
+        href={this.props.item.src}
+        className={`
+        grr-6 grc-6
+        grr-lg-${this.props.item.rows || 1} grc-lg-${this.props.item.cols || 1} 
+        pf-item with-overlay mf-popup`}
+        style={{
+        'backgroundImage': `url(${this.props.item.imgSrc})`
       }}>
-      <div className="overlay">
-        <div className="center-text">{this.props.item.sitename}<div className="subtext changed-pointer" dangerouslySetInnerHTML={{
-        __html: this.props.item.sitedesc || ""
-      }}></div>
+        <div className="overlay">
+          <div className="center-text">
+            {this.props.item.sitename}
+            <div
+              className="subtext changed-pointer"
+              dangerouslySetInnerHTML={{
+              __html: this.props.item.sitedesc || ""
+            }}></div>
+          </div>
         </div>
       </div>
-    </div>);
+    );
 
   };
 }
