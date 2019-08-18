d2bdfec114ede1ce3bfd84cd2b55be11a92d10cc
DaGuT <mangustalex11@gmail.com>
2019-08-18 13:28:38 +0100

    Moved ReplacebleComponent to file and made it universal

--- src/app/pages/index/parts/2.snakeBlock.js
+++ src/app/pages/index/parts/2.snakeBlock.js
@@ -2,6 +2,7 @@ import React from 'react';
 import p5 from 'p5';
 import snakeSketch from '../../../../canvas/snake/sketch';
 import TripletText from '../../../../misc/components/TripletText';
+import ReplacebleComponent from '../../../../misc/components/ReplacebleComponent';
 import './snakeBlock.css';
 
 let params = {
@@ -9,45 +10,24 @@ let params = {
   'blockID': 'Snake'
 };
 
-//Div that I need to pass as a Component property as initial component
-const Div = ({children,...props}) => (
-  <div {...props}>{children}</div>
-);
-
-//this class is used to create Component that will change its component upon click with a new one that is passed as props
-class ReplacableDesc extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      Component: Div,
-      params: {onClick:this.changeComponent}
+// Div from example in ReplacableComponent desc array of components that I've
+// describer above for replacablecomponent
+const components = [
+  {
+    component: 'div',
+    props: {
+      eventName: 'onClick'
+    }
+  }, {
+    component: TripletText,
+    props: {
+      eventName: 'none',
+      readMouseFrom: "#" + params.blockID,
+      relativeTo: "center",
+      id: "coolTripletTextInCenter"
     }
   }
-
-  changeComponent = () => {
-    this.setState({
-      Component: this.props.component,
-      params: {
-        readMouseFrom: "#" + params.blockID,
-        relativeTo: "center",
-        id: "coolTripletTextInCenter"
-      }
-    });
-  }
-
-  render() {
-    let Component = this.state.Component;
-    return (
-      <Component {...this.state.params}>
-        <div>
-          <h2>This site is about me and contains my (mini)projects</h2>
-          <h5>What you see now will change.It 's just a skeleton ATM to test myself</h5>
-        </div>
-      </Component>
-    );
-  }
-};
+]
 
 /**
  * snakeBlock - this is snakeBlock with heading of my site. It's private scope only
@@ -60,7 +40,14 @@ function snakeBlock() {
       id={params.blockID}
       key={params.blockID}
       className="text-center d-flex justify-content-center">
-      <div className="heading top align-self-center"><ReplacableDesc component={TripletText}/></div>
+      <div className="heading top align-self-center">
+        <ReplacebleComponent components={components}>
+          <div>
+            <h2>This site is about me and contains my (mini)projects</h2>
+            <h5>What you see now will change.It 's just a skeleton ATM to test myself</h5>
+          </div>
+        </ReplacebleComponent>
+      </div>
       <div className="top-right-text">
         <TripletText
           readMouseFrom={"#" + params.blockID}
---
+++ src/misc/components/ReplacebleComponent/ReplacebleComponent.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//components example:
+/*
+const components = [
+    {
+      component: 'div',
+      props: {
+        eventName: 'onClick'
+      }
+    }, {
+      component: TripletText,
+      props: {
+        eventName: 'none',
+        readMouseFrom: "#" + params.blockID,
+        relativeTo: "center",
+        id: "coolTripletTextInCenter"
+      }
+    }
+  ]
+*/
+
+// ----------------And now component itself this class is used to create
+// Component that will change its component upon click with a new one that is
+// passed as props
+export default class ReplacableComponent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // destructuring again so that we can take eventName and put it as prop name
+    // instead of prop value
+    let {
+      eventName,
+      ...restProps
+    } = this.props.components[0].props;
+
+    this.currentComponent = 0; // this is internal counter
+
+    this.state = {
+      Component: this.props.components[0].component,
+      props: {
+        //this one is responsible for changing components
+        [eventName]: this.changeComponent,
+        //all other props
+        ...restProps
+      }
+    }
+  }
+
+  changeComponent = () => {
+    let oldEvent = this.props.components[this.currentComponent].props.eventName; //I store it so that I can delete it
+    this.currentComponent = (this.currentComponent + 1) % this.props.components.length; //it's for looping thhrough all components that we've passed
+    //whoops, code repetition
+    let {
+      eventName,
+      ...props
+    } = this.props.components[this.currentComponent].props;
+    this.setState({
+      Component: this.props.components[this.currentComponent].component,
+      props: {
+        //as I mentioned before, we delete oldEvent
+        [oldEvent]: undefined,
+        //then we set new event for changing component
+        [eventName]: this.changeComponent,
+        //and we pass all other props
+        ...props
+      }
+    });
+  }
+
+  render() {
+    let Component = this.state.Component;
+    return (
+      <Component {...this.state.props}>{this.props.children}</Component>
+    );
+  }
+};
+
+ReplacableComponent.propTypes = {
+  components: PropTypes.arrayOf(PropTypes.shape({
+    component: PropTypes.any.isRequired, //it's just string or ReactComponent, I cant make it work with oneOfType
+    props: PropTypes.shape({eventName: PropTypes.string.isRequired})
+  }))
+}
